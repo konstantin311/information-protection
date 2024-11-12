0a0ce8e1fd3011119bc3f5c9ceaa9119d66eaed3
calculator <efremov.200328@gmail.com>
2024-11-12 10:39:28 +0700

    Edit poker.hpp

--- lab4/poker.hpp
+++ lab4/poker.hpp
@@ -1,3 +1,11 @@
 #include "lab1_lib.hpp"
 
+void shuffleDeck(std::vector<long long>& deck);
+
+void distributeCards(std::vector<long long>& deck, std::vector<std::vector<long long>>& playersCards, int playersCount, int countCards);
+
+long long decryptCard(long long encryptedCard, int playerIndex, const std::vector<long long>& D, int playersCount, long long p);
+
+std::string cardToString(int card);
+
 void test_poker();
